6eae4c40cb3526b9dc1ac331ce4a4bd678969ce6
Tohidul Islam <129326995+Zol-piE@users.noreply.github.com>
2025-01-07 23:43:21 +0600

    Create MEX Table.cpp

---
+++ Code-Forces/MEX Table.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fast ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define test int t; cin>>t;
+int main()
+{
+    fast
+    test
+    while(t--)
+    {
+        int n,m;
+        cin>>n>>m;
+        cout<<max(n,m)+1<<endl;
+
+    }
+    return 0;
+}
