5c1d1019af3cfa306878d6c2852924de1d56daf6
Tohidul Islam <129326995+Zol-piE@users.noreply.github.com>
2025-09-26 14:50:19 +0600

    Create B. Unconventional Pairs.cpp

---
+++ Code-Forces/B. Unconventional Pairs.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define int long long
+const int MOD = 1e9 + 7;
+const int INF = LLONG_MAX >> 1;
+#define rep(a,b) for(int i =a;i<b;i++)
+#define OP ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+signed main()
+{
+    int tc;
+    cin >> tc;
+    while (tc--)
+    {
+        int n; cin>>n;
+        vector<int> arr(n,0);
+        rep(0,n) cin>>arr[i];
+        sort(arr.begin(),arr.end());
+        int mx = INT_MIN;
+        for(int i =1;i<n;i+=2)
+        {
+        	if(i<n) mx = max(mx,arr[i]-arr[i-1]);
+        }
+        cout<<mx<<endl;
+    }
+    return 0;
+}
