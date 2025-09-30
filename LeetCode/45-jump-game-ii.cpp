44e4f574ebe377ebf9aa81c112349ee791cf1a76
Tohidul Islam <129326995+Zol-piE@users.noreply.github.com>
2025-09-30 16:06:59 +0600

    Update 45-jump-game-ii.cpp

--- LeetCode/45-jump-game-ii.cpp
+++ LeetCode/45-jump-game-ii.cpp
@@ -1,33 +1,47 @@
 class Solution {
 public:
-	class Solution {
-public:
-    int jump(vector<int>& nums,int pos = 0  ) {
+	int BFA(vector<int>& nums,int pos = 0  ) {
         int n = size(nums) -1;
         if(pos == n ) return 0;
         int m = 10001;
         for(int i =1; (pos+i)<= n && i<=nums[pos];i++)
         {
-        	m = min(m,1+ jump(nums,pos+i));
+        	m = min(m,1+ BFA(nums,pos+i));
         }
         return m;
     }
-};
-    int bfaMemo(vector<int>& nums,vector<int> &dp ,int pos = 0  ) { //Brute force Approach
-        if(pos == size(nums)) return 0;
-        if(dp[pos] !==100001 ) dp[pos];
+    int BfaMemo(vector<int>& nums,vector<int>&dp,int n ,int pos = 0 ) {
+        if(dp[pos]!=100001) return dp[pos];
         int m = 100001;
-        for(int i =1;i<=nums[pos];i++)
+        for(int i =1; (pos+i)<= n && i<=nums[pos];i++)
         {
-        	m = min(m,1+bfaMemo(nums,pos+i));
+        	m = min(m,1+ BfaMemo(nums,dp,n,pos+i));
         }
         dp[pos] = m;
         return dp[pos];
     }
-    int jump(vector<int>& nums) {
+    int jumpfun(vector<int>& nums) {
         int n = size(nums);
         vector<int> dp(n,100001);
-
+        dp[n-1] = 0;
+        int m = BfaMemo(nums,dp,n-1);
         return m;
+    }
+    int jump(vector<int>& nums) {
+        int   n = size(nums);
+        int i =0, m_pos = 0 , last = 0;
+        int j = 0;
+        while(last < n-1)
+        {
+        	m_pos =  max(m_pos,i + nums[i]);
+        	if( i == last )
+        	{
+        		last = m_pos; // changes then it finds the maximum possible pos
+        		j++;
+        	}
+        	i++; // iterate all items until last index is found
+        }
+        return j;
+
     }
 };
