2569449ab4dfdd8f2da3d99e7de02372bdd564a3
Tohidul Islam <129326995+Zol-piE@users.noreply.github.com>
2025-09-30 14:40:12 +0600

    Update 45-jump-game-ii.cpp

--- LeetCode/45-jump-game-ii.cpp
+++ LeetCode/45-jump-game-ii.cpp
@@ -1,22 +1,33 @@
 class Solution {
 public:
-	int bfa(vector<int>& nums,int pos = 0  ) { //Brute force Approach
-        if(pos == size(nums)) return 0;
-        int m = INT_MAX;
-        for(int i =1;i<=nums[pos];i++)
+	class Solution {
+public:
+    int jump(vector<int>& nums,int pos = 0  ) {
+        int n = size(nums) -1;
+        if(pos == n ) return 0;
+        int m = 10001;
+        for(int i =1; (pos+i)<= n && i<=nums[pos];i++)
         {
-        	m = min(m,1+bfa(nums,pos+i));
+        	m = min(m,1+ jump(nums,pos+i));
         }
         return m;
     }
-    
-    int jump(vector<int>& nums,int pos = 0  ) {
+};
+    int bfaMemo(vector<int>& nums,vector<int> &dp ,int pos = 0  ) { //Brute force Approach
         if(pos == size(nums)) return 0;
-        int m = INT_MAX;
+        if(dp[pos] !==100001 ) dp[pos];
+        int m = 100001;
         for(int i =1;i<=nums[pos];i++)
         {
-        	m = min(m,1+jump(nums,pos+i));
+        	m = min(m,1+bfaMemo(nums,pos+i));
         }
+        dp[pos] = m;
+        return dp[pos];
+    }
+    int jump(vector<int>& nums) {
+        int n = size(nums);
+        vector<int> dp(n,100001);
+
         return m;
     }
 };
