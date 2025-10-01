bc0870d2e79ef941e03d9a8503e656e57c2ad145
Tohidul Islam <129326995+Zol-piE@users.noreply.github.com>
2025-10-02 00:33:14 +0600

    Update 134-gas-station.cpp

--- LeetCode/134-gas-station.cpp
+++ LeetCode/134-gas-station.cpp
@@ -28,3 +28,26 @@ public:
         return 0;
     }
 };
+
+// second solve optimized
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int n = size(gas);
+        int g_sum = 0 ,c_sum =0;
+        int sum  =0 , s = 0;
+        for(int i =0;i<n;i++)
+        {
+            g_sum +=gas[i];
+            c_sum +=cost[i];
+            sum += gas[i] - cost[i];
+            if(sum<0)
+            {
+                s = i+1;
+                sum =0;
+            }
+        }
+        if(c_sum>g_sum) return -1;
+        return s;
+    }
+};
