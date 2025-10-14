85e49f3298f27abc354f35d02cf091664df67bbd
Tohidul Islam <129326995+Zol-piE@users.noreply.github.com>
2025-10-14 23:46:23 +0600

    Update B. Distinct Elements.cpp

--- Code-Forces/B. Distinct Elements.cpp
+++ Code-Forces/B. Distinct Elements.cpp
@@ -6,6 +6,11 @@ const int MOD = 1e9 + 7;
 const int INF = LLONG_MAX >> 1;
 #define rep(a,b) for(int i =a;i<b;i++)
 #define OP ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+void pr(vector<int> &arr,int n)
+{
+    for(int i =1;i<n;i++) cout<<arr[i]<<" ";
+    cout<<endl;
+}
 signed main()
 {
     int tc;
@@ -13,45 +18,34 @@ signed main()
     while (tc--)
     {
         int n; cin>>n;
+        n++;
         vector<int> arr(n,0);
-        rep(0,n) cin>>arr[i];
-        int mx = INT_MIN,mxi=0;
-        for(int i =0;i<n;i++)
+        rep(1,n) cin>>arr[i];
+
+        vector<int> num(n,1);
+
+        for(int i =2;i<n;i++) num[i] = i;
+        
+        vector<int> pSum(n,0);
+        pSum[1] = num[1];
+        rep(2,n)
         {
-        	if(arr[i] > mx)
-        	{
-        		mx = arr[i];
-        		mxi = i;
-        	}
+            pSum[i] = num[i] + pSum[i-1];
         }
-        int sum = (n*(n+1)) / 2;
-        vector<int>a(n,0);
-        int j =1;
-        // cout<<sum<<" "<<mx<<" "<<mxi<<" ";
-        for(int i =0;i<n;i++)
+
+        int c = 0;
+        for(int i =1;i<n;i++)
         {
- 			a[i] = j;
- 			j++;
+            if((pSum[i] - arr[i]) > 0)
+            {
+                c++;
+                num[i] = arr[i] - arr[i-1];
+            }
         }
-        int c = sum - mx;
-        // for(int x : a) cout<<x<<" ";
-        // cout<<endl;
-        if(c==1) 
-        {
-        	a[mxi] = 1;
-        	c =0;
-        }
-        for(int i =1;i<c;i++)
-        {
-        	int x = i;
-        	for(int j =n-1;x>0 && j>=0;j--)
-        	{
-        		a[j]--;
-        		x--;
-        	}
-        }
-        for(int x : a) cout<<x<<" ";
-        cout<<endl;
+        if(c==1) num[n-1] = pSum[n-1] - arr[n-1];
+        pr(num,n);
+
+
     }
     return 0;
 }
