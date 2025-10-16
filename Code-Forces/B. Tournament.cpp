5f40d0534050d1dcd1883587f01d15dc2901ce39
Tohidul Islam <129326995+Zol-piE@users.noreply.github.com>
2025-10-16 21:25:31 +0600

    Create B. Tournament.cpp

---
+++ Code-Forces/B. Tournament.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define int long long
+#define yes cout<<"YES"<<endl;
+#define no cout<<"NO"<<endl;
+const int MOD = 1e9 + 7;
+const int INF = LLONG_MAX >> 1;
+#define rep(a,b) for(int i =a;i<b;i++)
+#define OP ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+void pr(vector<int> &arr,int n)
+{
+    for(int i =0;i<n;i++) cout<<arr[i]<<" ";
+    cout<<endl;
+}
+signed main()
+{
+    int tc;
+    cin >> tc;
+    while (tc--)
+    {
+        int n,j,k; cin>>n>>j>>k;
+        vector<int> arr(n+1,0);
+        int elem  =0 ;
+        rep(1,n+1)
+        {
+        	cin>>arr[i];
+        	if(i ==j ) elem = arr[i];
+        }
+        sort(arr.begin(),arr.end());
+        if(k>j)
+        {
+        	yes
+        	continue;
+        }
+        int i;
+        for(i =1;i<=n;i++)
+        {
+        	if(arr[i] == elem)
+        	{
+        		break;
+        	}
+        }
+        if(k>=(n - i + 1))
+        {
+        	yes
+        	continue;
+        }
+        if(k == 2 || arr[n] == elem) yes
+       	else no
+    }
+    return 0;
+}
