3873d58e92d797c6c23339122843bdafc3d32771
Tohidul Islam <129326995+Zol-piE@users.noreply.github.com>
2025-10-17 23:36:51 +0600

    Create A. Beautiful Average.cpp

---
+++ Code-Forces/A. Beautiful Average.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define int long long
+#define yes cout<<"YES"<<endl;
+#define no cout<<"NO"<<endl;
+const int MOD = 1e9 + 7;
+const int INF = LLONG_MAX >> 1;
+#define rep(a,b) for(int i =a;i<b;i++)
+#define OP ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+void pr(vector<int> &arr,int n)
+{
+    for(int i =0;i<n;i++) cout<<arr[i]<<" ";
+    cout<<endl;
+}
+signed main()
+{
+    int tc;
+    cin >> tc;
+    while (tc--)
+    {
+        int n; cin>>n;
+        int mx = INT_MIN;
+        for(int i =0;i<n;i++)
+        {
+ 			int x; cin>>x;
+ 			if(x>mx) mx = x;
+        }
+        cout<<mx<<endl;
+    }
+    return 0;
+}
