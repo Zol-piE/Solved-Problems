95d8672c1fedbd7a71a406c8862d8dca04d4d334
Tohidul Islam <129326995+Zol-piE@users.noreply.github.com>
2025-10-17 23:36:54 +0600

    Create A. Way Too Long Words.cpp

---
+++ Code-Forces/A. Way Too Long Words.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define int long long
+#define yes cout<<"YES"<<endl;
+#define no cout<<"NO"<<endl;
+const int MOD = 1e9 + 7;
+const int INF = LLONG_MAX >> 1;
+#define rep(a,b) for(int i =a;i<b;i++)
+#define OP ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+void pr(vector<int> &arr,int n)
+{
+    for(int i =0;i<n;i++) cout<<arr[i]<<" ";
+    cout<<endl;
+}
+signed main()
+{
+    int tc;
+    cin >> tc;
+    while (tc--)
+    {
+        string str;
+        cin>>str;
+        int n = str.size();
+        if(n<=10) cout<<str<<endl;
+        else
+        {
+        	cout<<str[0]<<n-2<<str[n-1]<<endl;
+        }
+    }
+    return 0;
+}
