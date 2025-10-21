35a245ab8089a25ed22cd3fbbbb67c2871125cfa
Tohidul Islam <129326995+Zol-piE@users.noreply.github.com>
2025-10-21 21:59:07 +0600

    ig

--- Code-Forces/Test.cpp
+++ Code-Forces/Test.cpp
@@ -1,22 +1,35 @@
-#include <bits/stdc++.h>
+#include<iostream>
 using namespace std;
-#define endl '\n'
-#define int long long
-const int MOD = 1e9 + 7;
-const int INF = LLONG_MAX >> 1;
-#define rep(a,b) for(int i =a;i<b;i++)
-#define OP ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-signed main()
+void factors(int n)
 {
-	int n = 40;
-	vector<int>arr(n,0);
-	rep(0,n) cin>>arr[i];
-	vector<int>brr(n,0);
-	rep(0,n) cin>>brr[i];
-	rep(0,n)
-	{
-		if(arr[i]==brr[i] ) cout<<"Yes"<<endl;
-		else cout<<"No"<<endl;
-	}
-
+    if(n<=1) { cout<<1<<endl; return ;}
+    for(int i=2;i*i<=n;i++)
+    {
+        while(n%i==0)
+        {
+            cout<<i<<" ";
+            n/=i;
+        }
+    }
+    if(n>1) cout<<n<<endl;
+    else cout<<endl;
+}
+void opFactors(int n)
+{
+    if(n<=1) { cout<<1<<endl; return ;}
+    while(n%2==0) { cout<<2<<" "; n/=2; }
+    while(n%3==0) { cout<<3<<" "; n/=3; }
+    for(int i=5;i*i<=n;i+=6)
+    {
+        while(n%i==0) { cout<<i<<" ";  n/=i; }
+        while(n%(i+2)==0) { cout<<i+2<<" ";  n/=(i+2); }
+    }
+    if(n>3) cout<<n<<endl;
+    else cout<<endl;
+}
+int main()
+{
+    factors(450);
+    opFactors(450);
+    return 0;
 }
