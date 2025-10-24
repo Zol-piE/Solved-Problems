74c7075fced6a7d8733029fb6cb8df67777dc9df
Tohidul Islam <129326995+Zol-piE@users.noreply.github.com>
2025-10-24 23:01:37 +0600

    Create A. Pizza Time.cpp

---
+++ Code-Forces/A. Pizza Time.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define int long long
+#define yes cout<<"YES"<<endl;
+#define no cout<<"NO"<<endl;
+const int MOD = 1e9 + 7;
+const int INF = LLONG_MAX >> 1;
+#define rep(a,b) for(int i =a;i<b;i++)
+#define OP ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+void pr(vector<int> &arr,int n)
+{
+    for(int i =0;i<n;i++) cout<<arr[i]<<" ";
+    cout<<endl;
+}
+signed main()
+{
+    int tc;
+    cin >> tc;
+    while (tc--)
+    {
+        int n; cin>>n;
+        int ans  = 0;
+        while(n>2)
+        {
+        	ans += (n/3);
+        	int rem  = 0;
+        	if(n>3 ) rem = n%3;
+        	n = rem + (n / 3);
+        }
+        cout<<ans<<endl;
+    }
+    return 0;
+}
