741a02d3e921419dc86431fba833db42011cf645
Tohidul Islam <129326995+Zol-piE@users.noreply.github.com>
2025-10-25 00:06:38 +0600

    Update B. Strange Machine.cpp

--- Code-Forces/B. Strange Machine.cpp
+++ Code-Forces/B. Strange Machine.cpp
@@ -1,54 +1,58 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define endl '\n'
-// #define int long long
+#define int long long
 #define yes cout<<"YES"<<endl;
 #define no cout<<"NO"<<endl;
 const int MOD = 1e9 + 7;
-// const int INF = LLONG_MAX >> 1;
+const int INF = LLONG_MAX >> 1;
 #define rep(a,b) for(int i =a;i<b;i++)
 #define OP ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-
-int dp(int a, string &str, int n, map<int,int> &mem)
+void pr(vector<int> &arr,int n)
 {
-	if(a <= 0) return 0;
-	if(mem.find(a) != mem.end()) return mem[a];
-	int ans = 0;
-	int t = a;
-	while(a > 0)
-	{
-		int temp = a;
-		if(mem.find(a) != mem.end()) return mem[t] =  ans + mem[a];
-		for(int i = 0; i < n && a > 0; i++)
-		{
-			if(str[i] == 'A') a = a - 1;
-			else a = a / 2;
-			ans++;
-		}
-		mem[temp] = ans;
-	}
-	return mem[t] = ans;
+    for(int i =0;i<n;i++) cout<<arr[i]<<" ";
+    cout<<endl;
 }
-
 signed main()
 {
-    OP
     int tc;
     cin >> tc;
-    map<int,int> mem;
     while (tc--)
     {
-        int n, q; cin >> n >> q;
-        string str; cin >> str;
+        int n,q; cin>>n>>q;
+        string str; cin>>str;
 
-        mem.clear();
-        mem[0] = 0;
+        int b = count(str.begin(),str.end(),'B');
 
-        for(int i = 1; i <= q; i++)
-        {
-        	int a; cin >> a;
-        	cout << dp(a, str, n, mem) << endl;
-        }
+       for(int i =0;i<q;i++)
+       {
+       		int x; cin>>x;
+       		if(b== 0) 
+       		{
+       			cout<<x<<endl;
+       			continue;
+       		}
+       		int ans = 0;
+       		while(x>0)
+       		{
+       			for(int i =0;i<n;i++)
+       			{
+       				if(x<= 0) break;
+       				if(str[i] =='A')
+       				{
+       					ans++;
+       					x = x-1;
+       				}
+       				else
+       				{
+       					ans++;
+       					x= x/2;
+       				}
+       			}
+       		}
+       		cout<<ans<<endl;
+
+       }
     }
     return 0;
 }
