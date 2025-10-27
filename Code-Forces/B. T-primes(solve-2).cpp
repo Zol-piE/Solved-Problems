4582ef901440b6077f0f1841de99c4adac010418
Tohidul Islam <129326995+Zol-piE@users.noreply.github.com>
2025-10-27 23:47:03 +0600

    Create B. T-primes(solve-2).cpp

---
+++ Code-Forces/B. T-primes(solve-2).cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define int long long
+#define yes cout<<"YES"<<endl;
+#define no cout<<"NO"<<endl;
+const int MOD = 1e9 + 7;
+const int INF = LLONG_MAX >> 1;
+#define rep(a,b) for(int i =a;i<b;i++)
+#define OP ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+void pr(vector<int> &arr,int n)
+{
+    for(int i =0;i<n;i++) cout<<arr[i]<<" ";
+    cout<<endl;
+}
+signed main()
+{
+    int tc;
+    cin >> tc;
+    while (tc--)
+    {
+        int x; cin>>x;
+        
+    }
+    return 0;
+}
