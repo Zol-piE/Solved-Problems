f50cc5b238db8ee75b8c2cf2f82a49b06479dfb2
Tohidul Islam <129326995+Zol-piE@users.noreply.github.com>
2025-10-28 22:39:04 +0600

    Create B. Your Name.cpp

---
+++ Code-Forces/B. Your Name.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define int long long
+#define yes cout<<"YES"<<endl;
+#define no cout<<"NO"<<endl;
+const int MOD = 1e9 + 7;
+const int INF = LLONG_MAX >> 1;
+#define rep(a,b) for(int i =a;i<b;i++)
+#define OP ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+void pr(vector<int> &arr,int n)
+{
+    for(int i =0;i<n;i++) cout<<arr[i]<<" ";
+    cout<<endl;
+}
+signed main()
+{
+    int tc;
+    cin >> tc;
+    while (tc--)
+    {
+        int n; cin>>n;
+        string str,ptr;
+        cin>>str>>ptr;
+        sort(str.begin(),str.end());
+        sort(ptr.begin(),ptr.end());
+        bool l = false;
+        for(int i=0;i<n;i++)
+        {
+        	if(str[i] != ptr[i])
+        	{
+        		l = true;
+        		break;
+        	}
+        }
+        if(l) no
+        else yes
+
+    }
+    return 0;
+}
