31512a029c50fe8cefd8d9678435d108644c191a
Tohidul Islam <129326995+Zol-piE@users.noreply.github.com>
2025-10-31 19:25:46 +0600

    Create A. United We Stand.cpp

---
+++ Code-Forces/A. United We Stand.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define int long long
+#define yes cout<<"YES"<<endl;
+#define no cout<<"NO"<<endl;
+const int MOD = 1e9 + 7;
+const int INF = LLONG_MAX >> 1;
+#define rep(a,b) for(int i =a;i<b;i++)
+#define OP ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+void pr(vector<int> &arr,int n)
+{
+    for(int i =0;i<n;i++) cout<<arr[i]<<" ";
+    cout<<endl;
+}
+signed main()
+{
+    int tc;
+    cin >> tc;
+    while (tc--)
+    {
+        int n; cin>>n;
+        vector<int>arr,brr;
+        int x ; cin>>x;
+        arr.push_back(x);
+        for(int i=1;i<n;i++)
+        {
+        	cin>>x;
+        	bool l = true;
+        	for(int j: arr)
+        	{
+        		if(j % x == 0)
+        		{
+        			arr.push_back(x);
+        			l = false;
+        			break;
+        		}
+        	}
+        	if(l) brr.push_back(x);
+         }
+         int la = size(arr);
+         int lb = size(brr);
+         if(!la || !lb )
+         {
+         	cout<<-1<<endl;
+         }
+         else
+         {
+         	cout<<la<<" "<<lb<<endl;
+         	pr(arr,la);
+         	pr(brr,lb);
+         }
+
+    }
+    return 0;
+}
