4b42ebadb4cfa1e89c45095e9f2315f55f10f95c
Tohidul Islam <129326995+Zol-piE@users.noreply.github.com>
2025-11-29 16:19:24 +0600

    Update B. Even Modulo Pair.cpp

--- Code-Forces/B. Even Modulo Pair.cpp
+++ Code-Forces/B. Even Modulo Pair.cpp
@@ -2,74 +2,71 @@
 using namespace std;
 
 #define endl '\n'
-#define yes cout << "YES\n";
-#define no  cout << "NO\n";
-
 using ll = long long;
 
-const ll MOD = 1e9 + 7;
-const ll INF = LLONG_MAX >> 1;
+void solve_case() {
+    int n;
+    cin >> n;
+    vector<ll> a(n);
+    for (auto &x : a) cin >> x;
 
-#define rep(a,b) for(ll i = (a); i < (b); i++)
-#define ZolpiE ios_base::sync_with_stdio(false); cin.tie(nullptr);
+    sort(a.begin(), a.end());
 
-using vi = vector<int>;
-
-
-struct custom_hash {
-    static uint64_t splitmix64(uint64_t x) {
-        x += 0x9e3779b97f4a7c15;
-        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
-        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
-        return x ^ (x >> 31);
+    vector<ll> odd, ev;
+    for (ll x : a) {
+        if (x & 1) odd.push_back(x);
+        else ev.push_back(x);
     }
-    size_t operator()(uint64_t x) const {
-        static const uint64_t FIXED_RANDOM =
-            chrono::steady_clock::now().time_since_epoch().count();
-        return splitmix64(x + FIXED_RANDOM);
+
+    // CASE 1: Two evens → always YES
+    if (ev.size() >= 2) {
+        cout << ev[0] << " " << ev[1] << endl;
+        return;
     }
-};
-using ui = unordered_map<int,int,custom_hash>;
-ll sq(ll n) {
-    long double r = sqrtl((long double)n);
-    ll ans = (ll)r;
-    while ((__int128)ans * ans > n) ans--;
-    while ((__int128)(ans + 1) * (ans + 1) <= n) ans++;
-    return ans;
+
+    // CASE 2: One even → try modulo using it
+    if (ev.size() == 1) {
+        ll e = ev[0];
+        for (ll x : odd) {
+            if (x > e && ((x % e) % 2 == 0)) {
+                cout << e << " " << x << endl;
+                return;
+            }
+        }
+    }
+
+    // CASE 3: Only odd numbers
+    int m = odd.size();
+    if (m <= 1) {
+        cout << -1 << endl;
+        return;
+    }
+
+    sort(odd.begin(), odd.end());
+    ll V = odd.back();
+    int limit = log2(V) + 3;
+    int k = min(m, limit);
+
+    // Check all pairs among first k odd numbers
+    for (int i = 0; i < k-1; i++) {
+        for (int j = i + 1; j < k; j++) {
+            if (((odd[j] % odd[i]) % 2) == 0) {
+                cout << odd[i] << " " << odd[j] << endl;
+                return;
+            }
+        }
+    }
+
+    cout << -1 << endl;
 }
 
-void pr(vi &arr, int n) {
-    for (ll i = 0; i < n; i++) cout << arr[i] << " ";
-    cout << endl;
-}
-
-signed main() {
-    ZolpiE
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
 
     int tc;
     cin >> tc;
-    while (tc--) {
-
-    int n;cin>>n;
-    vi arr(n,0);
-    rep(0,n) cin>>arr[i];
-    bool  l =false;
-    for(int i =0;i<(n-1);i++)
-    {
-    	for(int j = i+1;j<n;j++)
-    	{
-    		if(!((arr[j] % arr[i]) & 1))
-    		{
-    			cout<<arr[i] << " "<<arr[j]<<endl;
-    			l = true;
-    			break;
-    		}
-    	}
-    		if(l) break;
-    }
-    if(!l) cout<<-1<<endl;
-
-    }
+    while (tc--) solve_case();
 
     return 0;
 }
