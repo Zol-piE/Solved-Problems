34b6cb89ea77eff28c14ada39d547ec9cfef4f3b
Tohidul Islam <129326995+Zol-piE@users.noreply.github.com>
2026-02-25 23:37:57 +0600

    Update B. Beautiful Numbers.cpp

--- Code-Forces/B. Beautiful Numbers.cpp
+++ Code-Forces/B. Beautiful Numbers.cpp
@@ -7,61 +7,92 @@ using namespace std;
 
 using ll = long long;
 
+const ll MOD = 1e9 + 7;
+const ll INF = LLONG_MAX >> 1;
+
+#define rep(a,b) for(ll i = (a); i < (b); i++)
 #define ZolpiE ios_base::sync_with_stdio(false); cin.tie(nullptr);
 
 using vi = vector<int>;
 
-vector<int> sumDigit(ll x)
-{
-    string s = to_string(x);
-    int n = (int)s.size();
 
-    vector<int> a(n + 1, 0);   // a[0] = digit sum, a[1..n] = digits (MSD..LSD)
-    for (int i = 0; i < n; i++) {
-        int d = s[i] - '0';
-        a[0] += d;
-        a[i + 1] = d;
+struct custom_hash {
+    static uint64_t splitmix64(uint64_t x) {
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
     }
-    return a;
+    size_t operator()(uint64_t x) const {
+        static const uint64_t FIXED_RANDOM =
+            chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
+};
+using ui = unordered_map<int,int,custom_hash>;
+ll sq(ll n) {
+    long double r = sqrtl((long double)n);
+    ll ans = (ll)r;
+    while ((__int128)ans * ans > n) ans--;
+    while ((__int128)(ans + 1) * (ans + 1) <= n) ans++;
+    return ans;
 }
 
+void pr(vi &arr, int n) {
+    for (ll i = 0; i < n; i++) cout << arr[i] << " ";
+    cout << endl;
+}
+vector<int> sumDigit(ll x)
+{
+    int n =  floor(log10(x)) + 1 ;
+
+    vector<int> sum(n+1,0);
+    int i = n;
+    while(x>0)
+    {
+        sum[0] += x%10;
+        sum[i--] = x%10;
+        x /=10;
+    }
+    return sum;
+}
 signed main() {
     ZolpiE
 
     int tc;
     cin >> tc;
     while (tc--) {
-        ll x;
-        cin >> x;
 
-        vector<int> arr = sumDigit(x);
-        int n = (int)arr.size(); // = digits + 1
+    ll x; cin>>x;
+    vector<int> arr = sumDigit(x);
 
-        if (arr[0] <= 9) {
-            cout << 0 << endl;
-            continue;
-        }
+    int n = arr.size();
 
-        // Build maximum possible reductions per move:
-        // - leading digit (arr[1]) can be reduced to at least 1 => reduction = d-1
-        // - other digits can be reduced to 0 => reduction = d
-        vector<int> red;
-        red.reserve(n - 1);
+    sort(arr.begin()+2,arr.end(),greater<int>());
+//
+//    rep(0,n) cout<<arr[i]<<" ";
+//    cout<<endl;
+     int c1 =0;
+     int sum = arr[0];
+     for(int i =2;i<n;i++)
+     {
+         if(arr[0]<= 9) break;
 
-        red.push_back(arr[1] - 1);          // leading digit
-        for (int i = 2; i < n; i++) red.push_back(arr[i]); // other digits
+         arr[0] -=arr[i];
+         c1++;
+     }
+     arr[0] = sum;
+     arr[0] = arr[0] - arr[1]+1;
+     int c2 = 1;
+     for(int i =2;i<n;i++)
+     {
+         if(arr[0]<= 9) break;
 
-        sort(red.begin(), red.end(), greater<int>());
+         arr[0] -=arr[i];
+         c2++;
+     }
+     cout<<min(c1,c2)<<endl;
 
-        int need = arr[0] - 9;
-        int got = 0, c = 0;
-        for (int r : red) {
-            if (got >= need) break;
-            got += r;
-            c++;
-        }
-
-        cout << c << endl;
     }
 
     return 0;
