a24fc62934dc29bc77b635f911150999fd86c899
Tohidul Islam <129326995+Zol-piE@users.noreply.github.com>
2026-07-02 22:18:44 +0600

    Create C. RemovevomeR.cpp

---
+++ Code-Forces/C. RemovevomeR.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define yes cout << "YES\n";
+#define no cout << "NO\n";
+using ll = long long;
+const ll MOD = 1e9 + 7;
+const ll INF = LLONG_MAX >> 1;
+#define rep(a, b) for (ll i = (a); i < (b); i++)
+#define ZolpiE                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(nullptr)
+using vi = vector<int>;
+struct custom_hash
+{
+    static uint64_t splitmix64(uint64_t x)
+    {
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+    size_t operator()(uint64_t x) const
+    {
+        static const uint64_t FIXED_RANDOM =
+            chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
+};
+void pr(vi &arr, int n)
+{
+    for (ll i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+void inputarray(vector<int> &arr, int n)
+{
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+}
+signed main()
+{
+    ZolpiE;
+
+    int tc;
+    cin >> tc;
+    while (tc--)
+    {
+
+        int n;
+        cin >> n;
+        string str;
+        cin >> str;
+        string nStr = "";
+        nStr += str[0];
+        int j = 0;
+        for (int i = 1; i < n; i++)
+        {
+            // cout << nStr << " " << str[i] << endl;
+            if (str[i] != nStr[j])
+            {
+                nStr += str[i];
+                j++;
+            }
+        }
+        int l = nStr.size();
+        if (l == 2 && nStr[0] != nStr[1])
+            cout << 2 << endl;
+        else
+            cout << 1 << endl;
+    }
+
+    return 0;
+}
