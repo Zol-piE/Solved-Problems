486f14a2b56172f91f97cd7d26ddff47106bbe72
Tohidul Islam <129326995+Zol-piE@users.noreply.github.com>
2026-07-02 22:18:39 +0600

    Create B. Good times Good times.cpp

---
+++ Code-Forces/B. Good times Good times.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define yes cout << "YES\n";
+#define no cout << "NO\n";
+using ll = long long;
+const ll MOD = 1e9 + 7;
+const ll INF = LLONG_MAX >> 1;
+#define rep(a, b) for (ll i = (a); i < (b); i++)
+#define ZolpiE                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(nullptr)
+using vi = vector<int>;
+struct custom_hash
+{
+    static uint64_t splitmix64(uint64_t x)
+    {
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+    size_t operator()(uint64_t x) const
+    {
+        static const uint64_t FIXED_RANDOM =
+            chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
+};
+void pr(vi &arr, int n)
+{
+    for (ll i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+void inputarray(vector<int> &arr, int n)
+{
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+}
+signed main()
+{
+    ZolpiE;
+
+    int tc;
+    cin >> tc;
+    while (tc--)
+    {
+        int x;
+        cin >> x;
+        int digit = floor(log10(x) + 1);
+        ll ans = 10;
+        while (--digit)
+        {
+            ans *= 10;
+        }
+        cout << ans + 1 << endl;
+    }
+
+    return 0;
+}
